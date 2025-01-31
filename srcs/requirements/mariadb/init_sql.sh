2131fab3c004767235faa6a41929fd5a4d7e141d
Matthias-Strauss <matthias.strauss@proton.me>
2025-01-31 13:15:39 +0100

    quicksave. gonna try something, dont want to lose current state.

--- srcs/requirements/mariadb/init_sql.sh
+++ srcs/requirements/mariadb/init_sql.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-mysql_install_db
+
+# load Credentials to env from secrets
+WP_USER=$(cat /run/secrets/wp_user)
+WP_PASSWORD=$(cat /run/secrets/wp_password)
+
+# finish setup
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi 
 mysqld
